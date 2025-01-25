7a64897a6e1745e2ce917e59619344c4352ad748
yvonneg254 <blessedangels254@gmail.com>
2025-01-25 12:54:09 +0300

    Add .gitattributes to manage line endings

--- config/db.js
+++ config/db.js
@@ -1,16 +1,20 @@
 // config/db.js
 const mongoose = require('mongoose');
+require('dotenv').config(); // Ensure dotenv is being used to load environment variables
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost/jkuat-mental-health', {
+        const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/MentalHub'; // Fall back to a default URI if the env variable is not set
+        console.log('Using MongoDB URI:', uri);  // Optional: To debug, check what URI is being used
+
+        await mongoose.connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
         console.log('Connected to MongoDB');
     } catch (err) {
         console.error('Error connecting to MongoDB', err);
-        process.exit(1);  // Exit the process if connection fails
+        process.exit(1);
     }
 };
 
